062c61e6a7dd903f1c1a758b26b9ff7a2216c881
jacobreppuhn <jakereppuhn@gmail.com>
2023-11-09 10:17:57 -0500

    Add job application routes, controller, and service

--- src/models/jobApplication.ts
+++ src/models/jobApplication.ts
@@ -1,59 +1,62 @@
-import { Model, DataTypes, Sequelize } from "sequelize";
-import { IApplicant, IJobApplication } from "../interfaces/ICommon";
+import { Model, DataTypes, Sequelize } from 'sequelize';
+import { IApplicant, IJobApplication } from '../interfaces/ICommon';
 
 export class JobApplication extends Model<IJobApplication> implements IJobApplication {
-  public jobApplicationId!: string;
-  public jobListingId!: string;
-  public applicationDate!: Date;
-  public applicationStatus!: Enumerator;
-  public applicationSource!: Enumerator;
-  public applicant!: IApplicant;
+	public jobApplicationId!: string;
+	public jobListingId!: string;
+	public applicationDate!: Date;
+	public applicationStatus!: Enumerator;
+	public applicationSource!: Enumerator;
+	public applicant!: IApplicant;
 
-  public static initialize(sequelize: Sequelize) {
-    JobApplication.init(
-      {
-        jobApplicationId: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          allowNull: false,
-          primaryKey: true,
-        },
-        jobListingId: {
-          type: DataTypes.UUID,
-          allowNull: false,
-        },
-        applicationDate: {
-          type: DataTypes.DATE,
-          allowNull: false,
-        },
-        applicationStatus: {
-          type: DataTypes.ENUM,
-          values: ["new", "archived"],
-          allowNull: false,
-        },
-        applicationSource: {
-          type: DataTypes.ENUM,
-          values: ["website", "indeed", "craigslist"],
-          allowNull: false,
-        },
-        applicant: {
-          type: DataTypes.JSON,
-          allowNull: false,
-        },
-      },
-      {
-        tableName: "jobApplications",
-        sequelize,
-      }
-    );
-  }
+	public static initialize(sequelize: Sequelize) {
+		JobApplication.init(
+			{
+				jobApplicationId: {
+					type: DataTypes.UUID,
+					defaultValue: DataTypes.UUIDV4,
+					allowNull: false,
+					primaryKey: true,
+				},
+				jobListingId: {
+					type: DataTypes.UUID,
+					allowNull: false,
+				},
+				applicationDate: {
+					type: DataTypes.DATE,
+					defaultValue: DataTypes.NOW,
+					allowNull: false,
+				},
+				applicationStatus: {
+					type: DataTypes.ENUM,
+					values: ['new', 'archived'],
+					defaultValue: 'new',
+					allowNull: false,
+				},
+				applicationSource: {
+					type: DataTypes.ENUM,
+					values: ['website', 'indeed'],
+					defaultValue: 'website',
+					allowNull: false,
+				},
+				applicant: {
+					type: DataTypes.JSON,
+					allowNull: false,
+				},
+			},
+			{
+				tableName: 'jobApplications',
+				sequelize,
+			}
+		);
+	}
 
-  public static associate(models: any) {
-    this.belongsTo(models.JobListing, {
-      foreignKey: "jobListingId",
-      as: "jobListing",
-    });
-  }
+	public static associate(models: any) {
+		this.belongsTo(models.JobListing, {
+			foreignKey: 'jobListingId',
+			as: 'jobListing',
+		});
+	}
 }
 
 export default JobApplication;
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,48 @@
-import http from "http";
-import express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import cors from "cors";
-import logging from "./config/logging";
-import db from "./models/index";
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import logging from './config/logging';
+import db from './models/index';
 
-import userRoutes from "./routes/userRoutes";
-import employeeRoutes from "./routes/employeeRoutes";
-import jobListingRoutes from "./routes/jobListingRoutes";
+import userRoutes from './routes/userRoutes';
+import employeeRoutes from './routes/employeeRoutes';
+import jobListingRoutes from './routes/jobListingRoutes';
+import jobApplicationRoutes from './routes/jobApplicationRoutes';
 
-const NAMESPACE = "Server";
+const NAMESPACE = 'Server';
 const router = express();
-const PORT: string = process.env.PORT || "3000";
-const NODE_ENV: string = process.env.NODE_ENV || "development";
+const PORT: string = process.env.PORT || '3000';
+const NODE_ENV: string = process.env.NODE_ENV || 'development';
 
 db.sequelize
-  .sync()
-  .then(() => {
-    logging.info(NAMESPACE, "Database synchronized");
-  })
-  .catch((err: Error) => {
-    logging.error(NAMESPACE, "Error synchronizing database", err);
-  });
+	.sync()
+	.then(() => {
+		logging.info(NAMESPACE, 'Database synchronized');
+	})
+	.catch((err: Error) => {
+		logging.error(NAMESPACE, 'Error synchronizing database', err);
+	});
 
 router.use((req, res, next) => {
-  logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+	logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
 
-  res.on("finish", () => {
-    logging.info(
-      NAMESPACE,
-      `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
-    );
-  });
+	res.on('finish', () => {
+		logging.info(
+			NAMESPACE,
+			`METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
+		);
+	});
 
-  next();
+	next();
 });
 
 router.use(
-  cors({
-    credentials: true,
-  })
+	cors({
+		credentials: true,
+	})
 );
 
 router.use(compression());
@@ -49,12 +50,13 @@ router.use(cookieParser());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.use("/api/v1/users", userRoutes);
-router.use("/api/v1/employees", employeeRoutes);
-router.use("/api/v1/job-listings", jobListingRoutes);
+router.use('/api/v1/users', userRoutes);
+router.use('/api/v1/employees', employeeRoutes);
+router.use('/api/v1/job-listings', jobListingRoutes);
+router.use('/api/v1/job-applications', jobApplicationRoutes);
 
 const app = http.createServer(router);
 
 app.listen(PORT, () => {
-  console.log(`Application running ${NODE_ENV} mode on port ${PORT}..`);
+	console.log(`Application running ${NODE_ENV} mode on port ${PORT}..`);
 });
