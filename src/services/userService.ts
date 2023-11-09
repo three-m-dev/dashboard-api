e312aa3e2a5037ff0749cac4d2c4a301f34783d3
jacobreppuhn <jakereppuhn@gmail.com>
2023-11-09 08:55:13 -0500

    Refactor user service module

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,132 +1,150 @@
-import User from "../models/user";
-import { sign } from "jsonwebtoken";
-import bcrypt from "bcryptjs";
-import { IUser, IUserDirectory, IUserInfo, IUserParams } from "../interfaces/ICommon";
-
-
+import User from '../models/user';
+import { sign } from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import { validate } from 'uuid';
+import {
+	IUser,
+	IUserDirectory,
+	IUserInfo,
+	IUserParams,
+} from '../interfaces/ICommon';
 
 export class UserService {
-  static async createUser(createdById: string, userData: IUser): Promise<IUserInfo> {
-    const requiredFields = ["username", "password", "accountType"] as const;
+	static async createUser(
+		createdById: string,
+		userData: IUser
+	): Promise<IUserInfo> {
+		const requiredFields = ['username', 'password', 'accountType'] as const;
 
-    const missingField = requiredFields.find((field) => !userData[field]);
+		const missingField = requiredFields.find((field) => !userData[field]);
 
-    if (missingField) {
-      throw new Error(`Missing required field: ${missingField}`);
-    }
+		if (missingField) {
+			throw new Error(`Missing required field: ${missingField}`);
+		}
 
-    const existingUser = await User.findAll({
-      where: { username: userData.username },
-    });
+		const existingUser = await User.findAll({
+			where: { username: userData.username },
+		});
 
-    if (existingUser.length > 0) {
-      throw new Error("Username already exists");
-    }
+		if (existingUser.length > 0) {
+			throw new Error('Username already exists');
+		}
 
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
+		const hashedPassword = await bcrypt.hash(userData.password, 10);
 
-    const user = await User.create({
-      ...userData,
-      password: hashedPassword,
-      createdBy: createdById,
-      updatedBy: createdById,
-    });
+		const user = await User.create({
+			...userData,
+			password: hashedPassword,
+			createdBy: createdById,
+			updatedBy: createdById,
+		});
 
-    const userInfo = {
-      userId: user.userId,
-      username: user.username,
-      accountType: user.accountType,
-      isActive: user.isActive,
-      createdBy: user.createdBy,
-      updatedBy: user.updatedBy,
-    };
+		const userInfo = {
+			userId: user.userId,
+			username: user.username,
+			accountType: user.accountType,
+			isActive: user.isActive,
+			createdBy: user.createdBy,
+			updatedBy: user.updatedBy,
+		};
 
-    return userInfo;
-  }
+		return userInfo;
+	}
 
-  static async authUser(username: string, password: string): Promise<{ accessToken: string }> {
-    if (!username || !password) {
-      throw new Error("Missing credentials");
-    }
+	static async authUser(
+		username: string,
+		password: string
+	): Promise<{ accessToken: string }> {
+		if (!username || !password) {
+			throw new Error('Missing credentials');
+		}
 
-    const user = await User.findOne({ where: { username } });
-    if (!user) {
-      throw new Error("User not found");
-    }
+		const user = await User.findOne({ where: { username } });
+		if (!user) {
+			throw new Error('User not found');
+		}
 
-    const passwordIsValid = await bcrypt.compare(password, user.password);
-    if (!passwordIsValid) {
-      throw new Error("Invalid password");
-    }
+		const passwordIsValid = await bcrypt.compare(password, user.password);
+		if (!passwordIsValid) {
+			throw new Error('Invalid password');
+		}
 
-    const accessToken = sign({ userId: user.userId, username: user.username }, process.env.JWT_SECRET as string, {
-      expiresIn: "24h",
-    });
+		const accessToken = sign(
+			{ userId: user.userId, username: user.username },
+			process.env.JWT_SECRET as string,
+			{
+				expiresIn: '24h',
+			}
+		);
 
-    return { accessToken };
-  }
+		return { accessToken };
+	}
 
-  static async getUsers(params: IUserParams): Promise<IUserDirectory> {
-    const { accountType, isActive } = params;
+	static async getUsers(params: IUserParams): Promise<IUserDirectory> {
+		const { accountType, isActive } = params;
 
-    const isActiveBoolean = isActive ? isActive === "true" : undefined;
+		const isActiveBoolean = isActive ? isActive === 'true' : undefined;
 
-    let whereConditions: any = {};
+		let whereConditions: any = {};
 
-    if (accountType) {
-      whereConditions.accountType = accountType;
-    }
+		if (accountType) {
+			whereConditions.accountType = accountType;
+		}
 
-    if (isActiveBoolean !== undefined) {
-      whereConditions.isActive = isActiveBoolean;
-    }
+		if (isActiveBoolean !== undefined) {
+			whereConditions.isActive = isActiveBoolean;
+		}
 
-    const users = await User.findAll({
-      where: whereConditions,
-      order: [["username", "ASC"]],
-    });
+		const users = await User.findAll({
+			where: whereConditions,
+			order: [['username', 'ASC']],
+		});
 
-    const userCount = users.length;
+		const userCount = users.length;
 
-    if (userCount === 0) {
-      throw new Error("No users found");
-    }
+		if (userCount === 0) {
+			throw new Error('No users found');
+		}
 
-    const userInfo = users.map((user) => ({
-      userId: user.userId,
-      username: user.username,
-      accountType: user.accountType,
-      isActive: user.isActive,
-      createdBy: user.createdBy,
-      updatedBy: user.updatedBy,
-    }));
+		const userInfo = users.map((user) => ({
+			userId: user.userId,
+			username: user.username,
+			accountType: user.accountType,
+			isActive: user.isActive,
+			createdBy: user.createdBy,
+			updatedBy: user.updatedBy,
+		}));
 
-    return {
-      users: userInfo,
-      count: userCount,
-    };
-  }
+		return {
+			users: userInfo,
+			count: userCount,
+		};
+	}
 
-  static async getUserById(userId: string): Promise<IUserInfo> {
-    if (userId === null) {
-      throw new Error("Invalid search criteria");
-    }
+	static async getUserById(userId: string): Promise<IUserInfo> {
+		if (!validate(userId)) {
+			throw new Error('Invalid search criteria');
+		}
 
-    const user = await User.findByPk(userId);
+		if (userId === null) {
+			throw new Error('User ID is required');
+		}
 
-    if (user === null) {
-      throw new Error("No user found");
-    }
+		const user = await User.findByPk(userId);
 
-    const userInfo = {
-      userId: user.userId,
-      username: user.username,
-      accountType: user.accountType,
-      isActive: user.isActive,
-      createdBy: user.createdBy,
-      updatedBy: user.updatedBy,
-    };
+		if (user === null) {
+			throw new Error('No user found');
+		}
 
-    return userInfo;
-  }
+		const userInfo = {
+			userId: user.userId,
+			username: user.username,
+			accountType: user.accountType,
+			isActive: user.isActive,
+			createdBy: user.createdBy,
+			updatedBy: user.updatedBy,
+		};
+
+		return userInfo;
+	}
 }
