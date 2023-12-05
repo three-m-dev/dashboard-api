8be9920219d47a1d47b2a14362b1e280b5b46b24
jacobreppuhn <jakereppuhn@gmail.com>
2023-12-05 17:39:17 -0500

    no https here

--- src/server.ts
+++ src/server.ts
@@ -1,89 +1,74 @@
-import fs from "fs";
-import path from "path";
-import https from "https";
-import express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import cors from "cors";
-import logger from "./utils/logger";
-import db from "./models/index";
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
 
-import { EmailService } from "./services/emailService";
+import logger from './utils/logger';
+import db from './models/index';
 
-import cameraRoutes from "./routes/cameraRoutes";
-import careerRoutes from "./routes/careerRoutes";
-import customerRoutes from "./routes/customerRoutes";
-import organizationRoutes from "./routes/organizationRoutes";
-import contentRoutes from "./routes/contentRoutes";
+import cameraRoutes from './routes/cameraRoutes';
+import careerRoutes from './routes/careerRoutes';
+import customerRoutes from './routes/customerRoutes';
+import organizationRoutes from './routes/organizationRoutes';
+import contentRoutes from './routes/contentRoutes';
 
-const NAMESPACE = "Server";
-const PORT: string = process.env.PORT || "8080";
-const NODE_ENV: string = process.env.NODE_ENV || "development";
+const NAMESPACE = 'Server';
+const PORT = process.env.PORT || '8080';
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
-const router = express();
+const app = express();
 
-EmailService.initializeTemplates();
-
-db.sequelize
-  .sync()
-  .then(() => {
-    logger.info(NAMESPACE, "Database synchronized");
-  })
-  .catch((err: Error) => {
-    logger.error(NAMESPACE, "Error synchronizing database", err);
-  });
-
-router.use((req, res, next) => {
-  logger.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
-
-  res.on("finish", () => {
-    logger.info(
-      NAMESPACE,
-      `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
-    );
-  });
-
-  next();
-});
-
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
-
-router.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Origin not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
-
-router.use(compression());
-router.use(cookieParser());
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
-router.use(express.static(__dirname + "/public"));
-
-router.use("/api/v1/cameras", cameraRoutes);
-router.use("/api/v1/careers", careerRoutes);
-router.use("/api/v1/content", contentRoutes);
-router.use("/api/v1/customers", customerRoutes);
-router.use("/api/v1/organization", organizationRoutes);
-
-const certificatesDirectory = path.join(__dirname, "..", "certificates");
-
-const options = {
-  key: fs.readFileSync(path.join(certificatesDirectory, "key.pem")),
-  cert: fs.readFileSync(path.join(certificatesDirectory, "cert.pem")),
+const corsOptions = {
+	origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+		if (!origin) {
+			callback(null, true);
+		} else {
+			callback(null, true);
+		}
+	},
+	credentials: true,
+	optionsSuccessStatus: 200,
 };
 
-const server = https.createServer(options, router);
+db.sequelize
+	.sync()
+	.then(() => {
+		logger.info(NAMESPACE, 'Database synchronized');
+	})
+	.catch((err: Error) => {
+		logger.error(NAMESPACE, 'Error synchronizing database', err);
+	});
+
+app.use((req, res, next) => {
+	logger.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+	res.on('finish', () => {
+		logger.info(
+			NAMESPACE,
+			`METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
+		);
+	});
+
+	next();
+});
+
+app.use(compression());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.use('/api/v1/cameras', cameraRoutes);
+app.use('/api/v1/careers', careerRoutes);
+app.use('/api/v1/content', contentRoutes);
+app.use('/api/v1/customers', customerRoutes);
+app.use('/api/v1/organization', organizationRoutes);
+
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Application running ${NODE_ENV} mode on port ${PORT}..`);
+	console.log(`Application running ${NODE_ENV} mode on port ${PORT}..`);
 });
