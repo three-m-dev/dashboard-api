21f86642b9229e774e2cf7e1963bc17807fcdf7d
Jacob Reppuhn <jreppuhn@three-m.com>
2023-12-13 10:16:56 -0500

    update interfaces

--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -1,108 +1,108 @@
 export interface ICommon {
-	id: string;
+  id: string;
 }
 
 export interface IAddress {
-	addressLine?: string;
-	city?: string;
-	state?: string;
-	country?: string;
-	postalCode?: string;
+  addressLine?: string;
+  city?: string;
+  state?: string;
+  country?: string;
+  postalCode?: string;
 }
 
 export interface IApplicant extends ICommon {
-	firstName: string;
-	lastName: string;
-	email: string;
-	phone: string;
-	resumeRef?: string;
-	resumeLink?: string;
-	answers: string[];
-	additionalInfo?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  resumeRef?: string;
+  resumeLink?: string;
+  answers?: string[];
+  additionalInfo?: string;
 }
 
 export interface IApplication extends ICommon {
-	jobId: string;
-	applicantId: string;
-	applicationDate: Date;
-	phoneScreenDate?: Date;
-	interviewDate?: Date;
-	status: Enumerator;
-	notes?: string;
-	ratings?: IRating[];
+  jobId?: string;
+  applicantId: string;
+  phoneScreenDate?: Date;
+  interviewDate?: Date;
+  status: Enumerator;
+  notes?: string;
+  ratings?: IRating[];
+  submittedAt: Date;
 }
 
 export interface ICompany extends ICommon {
-	addressId: string;
-	name: string;
+  addressId: string;
+  name: string;
 }
 
 export interface IDepartment extends ICommon {
-	companyId: string;
-	name: string;
+  companyId: string;
+  name: string;
 }
 
 export interface IEmployee extends ICommon {
-	userId: string;
-	firstName: string;
-	lastName: string;
-	email?: string;
-	phone?: string;
-	addressId?: string;
-	birthDate?: Date;
-	title: string;
-	departmentId: string;
-	directReportId: string;
-	type: Enumerator;
-	status: Enumerator;
-	startDate: Date;
-	endDate?: Date;
-	notes?: string;
-	createdBy: string;
-	updatedBy?: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email?: string;
+  phone?: string;
+  addressId?: string;
+  birthDate?: Date;
+  title: string;
+  departmentId: string;
+  directReportId: string;
+  type: Enumerator;
+  status: Enumerator;
+  startDate: Date;
+  endDate?: Date;
+  notes?: string;
+  createdBy: string;
+  updatedBy?: string;
 }
 
 export interface IJob extends ICommon {
-	companyId: string;
-	departmentId: string;
-	title: string;
-	description: string;
-	location: Enumerator;
-	type: Enumerator;
-	status: Enumerator;
-	benefits?: string[];
-	requirements?: string[];
-	qualifications?: string[];
-	schedule?: string[];
-	salary?: number;
-	wage?: number;
-	positionsOpen: number;
-	createdBy: string;
-	updatedBy?: string;
-	closingAt?: Date;
+  companyId: string;
+  departmentId: string;
+  title: string;
+  description: string;
+  location: Enumerator;
+  type: Enumerator;
+  status: Enumerator;
+  benefits?: string[];
+  requirements?: string[];
+  qualifications?: string[];
+  schedule?: string[];
+  salary?: number;
+  wage?: number;
+  positionsOpen: number;
+  createdBy: string;
+  updatedBy?: string;
+  closingAt?: Date;
 }
 
 export interface IQueryParams {
-	filter?: Record<string, any>;
-	sort?: string;
-	page?: number;
-	pageSize?: number;
-	fields?: string[];
+  filter?: Record<string, any>;
+  sort?: string;
+  page?: number;
+  pageSize?: number;
+  fields?: string[];
 }
 
 export interface IRating extends ICommon {
-	type: Enumerator;
-	value: number;
-	notes?: string;
-	createdBy: string;
+  type: Enumerator;
+  value: number;
+  notes?: string;
+  createdBy: string;
 }
 
 export interface IUser extends ICommon {
-	username: string;
-	password: string;
-	accountType: string;
-	permissionsId: string;
-	isActive: boolean;
+  username: string;
+  password: string;
+  accountType: string;
+  permissionsId: string;
+  isActive: boolean;
 }
 
-export interface IUserWithoutPassword extends Omit<IUser, 'password'> {}
+export interface IUserWithoutPassword extends Omit<IUser, "password"> {}
