706939c0561a03e43be86c47b1edb2e1277fd881
Jacob Reppuhn <jreppuhn@three-m.com>
2024-02-07 15:29:25 -0500

    gn

--- src/models/document.ts
+++ src/models/document.ts
@@ -1,4 +1,4 @@
-import { Model } from 'sequelize';
+import { Model, DataTypes, Sequelize } from 'sequelize';
 import { IDocument } from '../interfaces';
 
 class Document extends Model<IDocument> implements IDocument {
@@ -7,6 +7,36 @@ class Document extends Model<IDocument> implements IDocument {
   public ownerType!: string;
   public documentType!: string;
   public s3URL!: string;
+
+  public static initialize(sequelize: Sequelize) {
+    Document.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          allowNull: false,
+          primaryKey: true,
+        },
+        ownerId: {
+          type: DataTypes.UUID,
+          allowNull: true,
+        },
+        ownerType: {
+          type: DataTypes.UUID,
+          allowNull: false,
+        },
+        documentType: {
+          type: DataTypes.UUID,
+          allowNull: false,
+        },
+        s3URL: {
+          type: DataTypes.UUID,
+          allowNull: false,
+        },
+      },
+      { tableName: 'documents', underscored: true, sequelize }
+    );
+  }
 }
 
 export default Document;
