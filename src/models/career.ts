c6a330f9c4270fda7acb6aa94b28269ea7fd0405
Jacob Reppuhn <jreppuhn@three-m.com>
2024-03-11 07:44:13 -0400

    added career model

---
+++ src/models/career.ts
@@ -0,0 +1,82 @@
+import { Model, DataTypes, Sequelize } from 'sequelize';
+import { ICareer } from '../interfaces';
+
+class Career extends Model<ICareer> implements ICareer {
+  public id!: string;
+  public company!: string;
+  public title!: string;
+  public location!: string;
+  public employmentType!: string;
+  public salary!: string;
+  public description!: string;
+  public benefits!: string[];
+  public qualifications!: string[];
+  public responsibilities!: string[];
+  public schedule!: string[];
+  public questions!: string[];
+  public positionsOpen!: number;
+
+  public static initialize(sequelize: Sequelize) {
+    Career.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          allowNull: false,
+          primaryKey: true,
+        },
+        company: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        title: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        location: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        employmentType: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        salary: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        description: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        benefits: {
+          type: DataTypes.JSON,
+          allowNull: false,
+        },
+        qualifications: {
+          type: DataTypes.JSON,
+          allowNull: false,
+        },
+        responsibilities: {
+          type: DataTypes.JSON,
+          allowNull: false,
+        },
+        schedule: {
+          type: DataTypes.JSON,
+          allowNull: false,
+        },
+        questions: {
+          type: DataTypes.JSON,
+          allowNull: false,
+        },
+        positionsOpen: {
+          type: DataTypes.NUMBER,
+          allowNull: false,
+        },
+      },
+      { tableName: 'careers', underscored: true, sequelize }
+    );
+  }
+}
+
+export default Career;
